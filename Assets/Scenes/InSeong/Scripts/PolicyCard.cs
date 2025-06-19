ef773f6bed8eee846b40a8e662f6351194724a1c
ArcFarM <hinsung99@gmail.com>
2025-06-19 16:48:21 +0900

    스크립트 폴더 정리해서 위치 옮김

---
+++ Assets/Scenes/InSeong/Scripts/PolicyCard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MainGame.UI {
+    public class PolicyCard : MonoBehaviour {
+        #region Variables
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity Event Method
+        #endregion
+
+        #region Custom Method
+        #endregion
+    }
+
+}
