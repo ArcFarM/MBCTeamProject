53bd3aa00e1cc4ea7218abaa069127dc926543ea
ArcFarM <hinsung99@gmail.com>
2025-06-19 22:18:21 +0900

    정책 카드 적용 (진행중)

--- Assets/Scenes/InSeong/Scripts/PolicyCard.cs
+++ Assets/Scenes/InSeong/Scripts/PolicyCard.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainGame.UI {
     public class PolicyCard : MonoBehaviour {
         #region Variables
-
+        [SerializeField] Button acceptButton;
         #endregion
 
         #region Properties
         #endregion
 
         #region Unity Event Method
+        private void Start() {
+           
+        }
         #endregion
 
         #region Custom Method
+
         #endregion
     }
 
